c3d40f2ba6a0ca7ff806e35b778892c480a2f187
Gerald <gerald.schade@gmx.de>
2018-04-20 01:54:48 +0200

    Version 0.10734

--- labimp.cpp
+++ labimp.cpp
@@ -974,6 +974,8 @@ void hhcl::dverarbeit(const string& datei)
 						oNw.clear();
 					}
 					// z.B. "Labor 20101201 044232.dat"
+					/*auto*/chrono::system_clock::time_point aktzp=chrono::system_clock::now();
+					reing.hz("Aktzeit",&aktzp);
 					rpar.schreib(/*sammeln*/0,/*obverb*/1,/*idp*/0);
 					// z.B. "Labor 20101201 004634.dat"
 					rle.schreib(/*sammeln*/0,/*obverb*/1,/*idp*/0);
@@ -1074,6 +1076,8 @@ void hhcl::dverarbeit(const string& datei)
 					rpar.hz("oNw",oNw);
 					oNw.clear();
 				}
+				/*auto*/chrono::system_clock::time_point aktzp=chrono::system_clock::now();
+				reing.hz("Aktzeit",&aktzp);
 				// z.B. "Labor 20101202 002036.dat"
 				rpar.schreib(/*sammeln*/0,/*obverb*/1,/*idp*/0);
 				// z.B. "Labor 20101202 011636.dat"
