22eb7c570ad893ff0f2b9d9b23627319ca4a1c75
Gerald <gerald.schade@gmx.de>
2018-04-20 18:31:54 +0200

    Version 0.10745

--- DB.cpp
+++ DB.cpp
@@ -967,6 +967,7 @@ int Tabelle::machind(const size_t aktc, int obverb/*=0*/, int oblog/*=0*/)
 			for(unsigned j=0;j<indx->feldzahl;j++) {
 				sql<<"`"<<indx->felder[j].name<<"`";
 				for(unsigned spnr=0;spnr<spalt->num_rows;spnr++) { // reale Spalten
+					caus<<rot<<indx->felder[j].name<<schwarz<<", spnamen: "<<spnamen[spnr]<<", spnr: "<<spnr<<schwarz<<endl;
 					if (!strcasecmp(indx->felder[j].name.c_str(),spnamen[spnr])) { // Feldnamen identisch
 //						if (indx->felder[j].lenge.empty()) indx->felder[j].lenge=splenge[spnr];
 						const long numsplen=atol(splenge[spnr]);
